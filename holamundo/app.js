fb90c9af9bbade887419faaeb945aba2d8d911c3
Jimenez Barboza Aridai Monserrath <jimenezari09@gmail.com>
2024-09-23 11:51:20 -0600

    Agrega el arreglo Tasks y filtra las tareas por realizar

---
+++ holamundo/app.js
@@ -0,0 +1,25 @@
+const {otroMensaje} = require("./hola")
+
+console.log("Este es el punto de entrada")
+console.log(otroMensaje)
+
+const tarea = [
+    {
+        nombre: "Estudiar para el examen de Backend",
+        fecha: "2024-09-23"
+        hecho: false
+    },
+    {
+        nombre: "Ir de compras",
+        fecha: "23/09/2024",
+        hecho: false,
+    },
+    {
+            nombre: "Hacer tarea",
+            fecha: "2024-09-22",
+            hecho: true,
+    },
+];
+
+const tareasPorRealizar = tareas.filter(tarea => !tarea.echo);
+console.log(tareasPorRealizar);
